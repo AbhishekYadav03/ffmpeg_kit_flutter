414704af5b3351a3cfb5a378d95102e2b3d91283
Abhishek <abhishek@datavoice.co.in>
2025-05-14 18:40:11 +0530

    changes

---
+++ android/src/main/java/com/antonkarpenko/ffmpegkit/FfmpegKitFlutterPlugin.java
@@ -0,0 +1,29 @@
+package com.antonkarpenko.ffmpegkit;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
+public class FfmpegKitFlutterPlugin implements FlutterPlugin, MethodCallHandler {
+  private MethodChannel channel;
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "ffmpeg_kit_flutter");
+    channel.setMethodCallHandler(this);
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
+
+  @Override
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    result.notImplemented(); // Replace with actual method handling if needed
+  }
+}
